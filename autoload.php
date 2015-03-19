509f9daf02ca07e096f17ed1dbea2ee8f94b7f8f
xetamus <shishamin@gmail.com>
2015-03-19 00:42:17 -0400

    fix up index pages api processing

--- autoload.php
+++ autoload.php
@@ -1,5 +1,7 @@
 <?php
 
+// I'm used to run tests. Don't forget to add libs to me before running the
+// full testsuite
 include_once "www/lib/fibonacci.php";
 include_once "www/lib/restAPI.php";
 
--- www/index.php
+++ www/index.php
@@ -3,4 +3,10 @@ require "lib/restAPI.php";
 
 $API = new restAPI($_GET['request']);
 
+if ( $API->response ) {
+    echo $API->response;
+} else {
+    $API->processRequest();
+}
+
 ?>
